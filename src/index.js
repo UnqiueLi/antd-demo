726ba46d620c7f96ad6791be1654cc71672aa523
lipenghui <lipenghui@hes0501.com.cn>
2021-07-14 11:31:16 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 
 /**
  * JSX只是react提供的一个语法糖
+ * 老的版本需要引入react 变量
+ * 但是新的版本不需要引入 
+ * import React from 'react';
  */
 ReactDOM.render(
   <h1>hello</h1>,document.getElementById('root')
